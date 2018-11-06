97f7a3c6d45394987e01025c46e7738cf28f5896
ololokiras <bellendirr@yandex.ru>
2018-11-06 21:13:15 +0500

    linted

--- src/plugins/assets.js
+++ src/plugins/assets.js
@@ -1,5 +1,5 @@
 const fetchingAssets = store => {
-  store.subscribe((mutation, state) => {
+  store.subscribe((mutation) => {
     if (mutation.type === 'acc/FETCH_CURRENT_USER') {
       const assets = Object.keys(mutation.payload.data.balances);
       store.dispatch('assets/fetchAssets', { assets }, { root: true });
