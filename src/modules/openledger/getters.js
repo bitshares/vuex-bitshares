50fd48a7f255f63e3398b0ef9c30544ae6dab066
roma219 <roma219@yandex.ru>
2018-12-10 12:32:45 +0300

    [openledger] pending list getter

--- src/modules/openledger/getters.js
+++ src/modules/openledger/getters.js
@@ -1,7 +1,8 @@
 const getters = {
   getDepositAddress: state => state.depositAddress,
   getCoinsData: state => state.coins,
-  getAddressPending: state => state.pendingAddress
+  getAddressPending: state => state.pendingAddress,
+  getCoinsPending: state => state.pending
 };
 
 export default getters;
