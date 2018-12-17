10fa685c43152bb4de1663c00ad6cafb00d91b85
ololokiras <bellendirr@yandex.ru>
2018-12-17 12:10:27 +0500

    move parse orders to ui

--- src/modules/acc/actions.js
+++ src/modules/acc/actions.js
@@ -11,52 +11,6 @@ const balancesToObject = (balancesArr) => {
   return obj;
 };
 
-const parseOpenOrders = (orders, rootGetters) => {
-  const parsedActiveOrders = orders.map(order => {
-    // need to get if from somewhere
-    const isBid = true;
-
-    const payInSellPrice = isBid ? order.sell_price.base : order.sell_price.quote;
-    const payAssetId = payInSellPrice.asset_id;
-    const payAsset = rootGetters['assets/getAssetById'](payAssetId);
-
-    const receiveInSellPrice = isBid ? order.sell_price.quote : order.sell_price.base;
-    const receiveAssetId = receiveInSellPrice.asset_id;
-    const receiveAsset = rootGetters['assets/getAssetById'](receiveAssetId);
-
-    const get = isBid
-      ? parseFloat(receiveInSellPrice.amount) / (10 ** receiveAsset.precision)
-      : parseFloat(payInSellPrice.amount) / (10 ** payAsset.precision);
-    const spend = isBid
-      ? parseFloat(payInSellPrice.amount) / (10 ** payAsset.precision)
-      : parseFloat(receiveInSellPrice.amount) / (10 ** receiveAsset.precision);
-
-    const payAssetSymbol = isBid ? payAsset.symbol : receiveAsset.symbol;
-    const receiveAssetSymbol = isBid ? receiveAsset.symbol : payAsset.symbol;
-    const filled = (order.sell_price.base.amount - order.for_sale) / (order.sell_price.base.amount);
-
-    const price = isBid
-      ? parseFloat(spend / get)
-      : parseFloat(get / spend);
-
-    const expiration = (new Date(order.expiration)).getTime();
-
-    return {
-      payAssetSymbol,
-      receiveAssetSymbol,
-      expiration,
-      get,
-      order: isBid ? 'buy' : 'sell',
-      vol: isBid ? get : spend,
-      spend,
-      price,
-      filled,
-      orderId: order.id
-    };
-  });
-  return parsedActiveOrders;
-};
-
 const actions = {
   /**
    * Logs in with password
@@ -189,14 +143,13 @@ const actions = {
   },
 
   fetchCurrentUser: async (store) => {
-    const { commit, getters, rootGetters } = store;
+    const { commit, getters } = store;
     const userId = getters.getAccountUserId;
     if (!userId) return;
     const result = await API.Account.getUser(userId);
     if (result.success) {
       const user = result.data;
       result.data.balances = balancesToObject(user.balances);
-      user.limit_orders = parseOpenOrders(user.limit_orders, rootGetters);
       commit(types.FETCH_CURRENT_USER, { data: user });
     }
   },
