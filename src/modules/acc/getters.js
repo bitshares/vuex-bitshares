4714f452e1d2ed1c374320ad0ed7e56fc1416be0
ololokiras <bellendirr@yandex.ru>
2018-12-21 16:03:31 +0300

    getter repair

--- src/modules/acc/getters.js
+++ src/modules/acc/getters.js
@@ -56,7 +56,7 @@ const getters = {
     return state.wallet.aesPrivate.decryptHexToText(state.wallet.encryptedBrainkey);
   },
   getCurrentUserName: state => {
-    return (state.userData && state.userData.account.name) || null;
+    return state.userData && state.userData.account.name;
   },
   getUserBalances: state => {
     if (!state.userData || !state.userData.balances) return {};
